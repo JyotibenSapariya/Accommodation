6ee0370dd4a78e77a99b2b7e958a56749ab62539
JyotibenSapariya <sapriyajyoti10@gmail.com>
2018-01-07 02:38:13 +0100

    add login page

---
+++ app/components/children/Login.jsx
@@ -0,0 +1,15 @@
+// Include React
+var React = require("react");
+
+var login = React.createClass({
+
+    render: function(){
+        return (
+            <div id="landingWindow">
+                <p id="subTitle">Login</p>
+            </div>
+        )
+    }
+});
+
+module.exports = login;

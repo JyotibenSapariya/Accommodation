1a4ee11f88573eedd3ae09ac696b19dee8b736d8
JyotibenSapariya <sapriyajyoti10@gmail.com>
2018-01-07 02:37:37 +0100

    find and rent room link added

---
+++ app/components/children/Rent.jsx
@@ -0,0 +1,15 @@
+// Include React
+var React = require("react");
+
+var Rent = React.createClass({
+
+    render: function(){
+        return (
+            <div id="landingWindow">
+                <p id="subTitle">Rent a room</p>
+            </div>
+        )
+    }
+});
+
+module.exports = Rent;
